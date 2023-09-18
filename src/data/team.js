4aece5aebbeb7886a5e6de4c665faf4f8e64d59b
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-09-18 15:41:25 +0100

    Update team.js

--- src/data/team.js
+++ src/data/team.js
@@ -1,5 +1,6 @@
 import AkhmimImg from "/team/akhmim.webp";
 import KevinImg from "/team/kevin.webp";
+import RichardImg from "/team/richard.webp";
 const team = [
   {
     name: "Abdelilah Akhmim",
@@ -16,6 +17,14 @@ const team = [
     linkedIn: "https://twitter.com/thepatch_kev",
     github: "https://github.com/betweentwomidnights",
     discord: "https://discordapp.com/users/138110479380905984",
+  },
+  {
+    name: "Richard Porteous",
+    job: "Computer Programmer",
+    image: RichardImg,
+    linkedIn: "https://www.linkedin.com/in/richard-porteous-168389248/",
+    github: "https://github.com/bakobiibizo",
+    discord: "https://discordapp.com/users/288880607780667394",
   },
 ];
 export default team;
