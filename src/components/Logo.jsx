2a1bafcecf8604fe40f6035ba2800f4814ef895a
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-10-13 16:25:36 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ function Logo() {
             {/* <img className="w-10 h-10 mx-1 rounded-md" src="logo.png" alt="logo" /> */}
             <div className="">
                 <span className="text-white text-3xl font-bold font-Gugi text-firstLogo">
-                Sonic<span className="font-Gugi text-secondLogo">Meow</span>
+                AISmart<span className="font-Gugi text-secondLogo">TaskCoder</span>
                 </span>
                 <span className="text-white text-3xl font-light font-Iceland text-thirdLogo">
                     {/* Craft */}
