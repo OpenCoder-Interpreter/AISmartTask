951b82cead4289ad06957877a13d121ecead6491
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-10-13 16:24:32 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
                 <Logo />
                 <div className="py-5 text-secondText text-base font-normal flex-box">
                     <CopyrightIcon />
-                    2023 SonicMeow - All Right Reserved
+                    2023 AISmartTaskCoder - All Right Reserved
                 </div>
                 <div className="py-3">
                     <div className="flex-box text-white">
